6c7512112c3edc1b1e609b4a5d88cb35bc8693da
Quann03 <97352012+Quann03@users.noreply.github.com>
2022-01-08 22:43:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi cho Crush...<3",
+    introTitle: 'Bạn Đình Quân <',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
