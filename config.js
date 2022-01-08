a130f479ea178999a61cd7f25662627cd199fcbc
Quann03 <97352012+Quann03@users.noreply.github.com>
2022-01-08 22:53:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
     titleWeb: "Gửi cho Crush...<3",
-    introTitle: 'Bạn Đình Quân <',
+    introTitle: 'Bạn Văn,Phương,Vinh,Chiến,Nghĩa,Tuấn Anh <',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Test thử tí ae  🥰',
+    desc: 'Hihi ',
+    btnYes: 'Có Cái Nịt<33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: https://l.facebook.com/l.php?u=https%3A%2F%2Fm.me%2FHi.Quann.03%3Ffbclid%3DIwAR0FEMofK59IVsg8Mn09BXavB9s1cixR-SbtMck8Ll6-AJI4at4XgIbx8hQ&h=AT1S4JGcoFdb2nbFfOiQ5VVh8i_iYNv-6kbtcvUQJKeSdTeZjucWdUlx5hVbLVxIU13SbGZ3VQJXXQBirVYe-cPMKBQdq5J4TEiRBMzbw2UKi4Gfp-LhEcjSDIYA30y8EEP_
 }
